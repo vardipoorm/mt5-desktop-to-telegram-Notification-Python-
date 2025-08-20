e916bc58638c9393eba44f01effd45ef32650da3
vardipoorm <84905074+vardipoorm@users.noreply.github.com>
2025-08-20 21:19:51 +0330

    Delete run python mt5-telegram.bat

--- run python mt5-telegram.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Running Python script...
-
-:: این دستور فایل پایتون را از همان پوشه فعلی اجرا می‌کند
-python "%~dp0bot_script.py"
-
-echo Script finished. Press any key to close.
-pause
